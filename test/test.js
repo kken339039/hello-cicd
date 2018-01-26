d4cd3949b43d18e762af890d9cafd6e5978dc8dd
KellenCheng <kken339039@yahoo.com.tw>
2018-01-26 11:34:54 +0800

    fix test.js

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var assert = require('assert')
 describe('Array', function () {
   describe('#indexOf()', function () {
     it('Return -1 if element can not be searched', function () {
-      assert.equal(0, [1, 2, 3].indexOf(0)) //假裝test不小心寫錯囉
+      assert.equal(-1, [1, 2, 3].indexOf(0)) //改回來
     })
   })
 })
