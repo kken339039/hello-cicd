cf85b9171fa27c79df4262b08bb8e8f2686f175a
KellenCheng <kken339039@yahoo.com.tw>
2018-01-30 15:04:40 +0800

    add vue templete and webpack

--- test/test.js
+++
@@ -1,8 +0,0 @@
-var assert = require('assert')
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('Return -1 if element can not be searched', function () {
-      assert.equal(-1, [1, 2, 3].indexOf(0)) //改回來
-    })
-  })
-})
